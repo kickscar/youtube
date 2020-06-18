273ca5fc0f8b68a3a2a1c487ab367195030a32bb
kickscar <kickscar@gmail.com>
2020-06-18 16:36:01 +0900

    구글라이브러리 버리기#2

--- api/practices/src/main/java/me/kickscar/youtube/api/without/googlelibs/Search.java
+++ api/practices/src/main/java/me/kickscar/youtube/api/without/googlelibs/Search.java
@@ -1,19 +1,15 @@
 package me.kickscar.youtube.api.without.googlelibs;
 
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.ResourceId;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
-import com.google.api.services.youtube.model.Thumbnail;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.iv.RandomIvGenerator;
 import org.jasypt.properties.EncryptableProperties;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class Search {
@@ -23,6 +19,8 @@ public class Search {
     private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
 
     public static void main(String[] args) throws Throwable {
+
+        // 1. api key
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword("KICKSCAE_ME");
         encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
@@ -36,48 +34,31 @@ public class Search {
         properties.load(Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME));
         String apiKey = properties.getProperty("youtube.apikey");
 
-        YouTube youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+        // 2. sending request
+        String spec = new StringBuilder("https://www.googleapis.com/youtube/v3/search")
+                .append("?")
+                .append("part=").append("snippet").append("&")
+                .append("order=").append("viewCount").append("&")
+                .append("maxResults=").append(NUMBER_OF_VIDEOS_RETURNED).append("&")
+                .append("q=").append(QUERY_TERM).append("&")
+                .append("type=").append("video").append("&")
+                .append("videoDefinition=").append("high").append("&")
+                .append("key=").append(apiKey)
+                .toString();
 
-            public void initialize(HttpRequest request) throws IOException {
-            }
+        HttpURLConnection httpURLConnection = (HttpURLConnection)(new URL(spec).openConnection());
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.setRequestProperty("Accept", "application/json");
 
-        }).setApplicationName("yt-crawler-functional-prototype").build();
-
-        YouTube.Search.List search = youtube.search().list("id,snippet");
-        search.setKey(apiKey);
-        search.setQ(QUERY_TERM);
-        search.setType("video");
-        search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-        search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-
-        SearchListResponse searchResponse = search.execute();
-        List<SearchResult> searchResultList = searchResponse.getItems();
-        if (searchResultList != null) {
-            prettyPrint(searchResultList.iterator(), QUERY_TERM);
-        }
-    }
-
-    private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
-        System.out.println("\n=============================================================");
-        System.out.println("   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
-        System.out.println("=============================================================\n");
-
-        if (!iteratorSearchResults.hasNext()) {
-            System.out.println(" There aren't any results for your query.");
+        // 3. processing response
+        int responseCode = httpURLConnection.getResponseCode();
+        if(responseCode != 200) {
+            String responseMessage = httpURLConnection.getResponseMessage();
+            System.out.println(responseCode + " " + responseMessage);
+            return;
         }
 
-        while (iteratorSearchResults.hasNext()) {
-            SearchResult singleVideo = iteratorSearchResults.next();
-            ResourceId rId = singleVideo.getId();
-
-            if (rId.getKind().equals("youtube#video")) {
-                Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
-
-                System.out.println(" Video Id" + rId.getVideoId());
-                System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
-                System.out.println(" Thumbnail: " + thumbnail.getUrl());
-                System.out.println("\n-------------------------------------------------------------\n");
-            }
-        }
+        Map map = new ObjectMapper().readValue(new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8"), HashMap.class);
+        System.out.println(map);
     }
 }
